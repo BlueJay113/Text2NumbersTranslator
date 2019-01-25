1b76c797815a36346065d95fc7646d0a6276d08e
BlueJay <thechosenone113@outlook.com>
2019-01-25 20:21:55 +1000

    Add files via upload

---
+++ Translator/Translator/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.translateNumbers = new System.Windows.Forms.Button();
+            this.translateText = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 397);
+            this.textBox1.TabIndex = 0;
+            // 
+            // translateNumbers
+            // 
+            this.translateNumbers.Location = new System.Drawing.Point(195, 416);
+            this.translateNumbers.Name = "translateNumbers";
+            this.translateNumbers.Size = new System.Drawing.Size(189, 35);
+            this.translateNumbers.TabIndex = 2;
+            this.translateNumbers.Text = "Translate to Numbers";
+            this.translateNumbers.UseVisualStyleBackColor = true;
+            this.translateNumbers.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // translateText
+            // 
+            this.translateText.Location = new System.Drawing.Point(395, 416);
+            this.translateText.Name = "translateText";
+            this.translateText.Size = new System.Drawing.Size(163, 35);
+            this.translateText.TabIndex = 3;
+            this.translateText.Text = "Translate to Text";
+            this.translateText.UseVisualStyleBackColor = true;
+            this.translateText.Click += new System.EventHandler(this.translateText_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(710, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Replace numbers (in integers) to names for it to translate properly. Capitalisati" +
+    "on will be lost.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 444);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Translator by BlueJaySoftware";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translateText);
+            this.Controls.Add(this.translateNumbers);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(822, 506);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(822, 506);
+            this.Name = "Form1";
+            this.Text = "Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button translateNumbers;
+        private System.Windows.Forms.Button translateText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Translator/Translator/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        public string text2translate;
+        public string result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("A", "[0]");
+            textBox1.Text = textBox1.Text.Replace("a", "[0]");
+            textBox1.Text = textBox1.Text.Replace("B", "[1]");
+            textBox1.Text = textBox1.Text.Replace("b", "[1]");
+            textBox1.Text = textBox1.Text.Replace("C", "[2]");
+            textBox1.Text = textBox1.Text.Replace("c", "[2]");
+            textBox1.Text = textBox1.Text.Replace("D", "[3]");
+            textBox1.Text = textBox1.Text.Replace("d", "[3]");
+            textBox1.Text = textBox1.Text.Replace("E", "[4]");
+            textBox1.Text = textBox1.Text.Replace("e", "[4]");
+            textBox1.Text = textBox1.Text.Replace("F", "[5]");
+            textBox1.Text = textBox1.Text.Replace("f", "[5]");
+            textBox1.Text = textBox1.Text.Replace("G", "[6]");
+            textBox1.Text = textBox1.Text.Replace("g", "[6]");
+            textBox1.Text = textBox1.Text.Replace("H", "[7]");
+            textBox1.Text = textBox1.Text.Replace("h", "[7]");
+            textBox1.Text = textBox1.Text.Replace("I", "[8]");
+            textBox1.Text = textBox1.Text.Replace("i", "[8]");
+            textBox1.Text = textBox1.Text.Replace("J", "[9]");
+            textBox1.Text = textBox1.Text.Replace("j", "[9]");
+            textBox1.Text = textBox1.Text.Replace("K", "(10)");
+            textBox1.Text = textBox1.Text.Replace("k", "(10)");
+            textBox1.Text = textBox1.Text.Replace("L", "(11)");
+            textBox1.Text = textBox1.Text.Replace("l", "(11)");
+            textBox1.Text = textBox1.Text.Replace("M", "(12)");
+            textBox1.Text = textBox1.Text.Replace("m", "(12)");
+            textBox1.Text = textBox1.Text.Replace("N", "(13)");
+            textBox1.Text = textBox1.Text.Replace("n", "(13)");
+            textBox1.Text = textBox1.Text.Replace("O", "(14)");
+            textBox1.Text = textBox1.Text.Replace("o", "(14)");
+            textBox1.Text = textBox1.Text.Replace("P", "(15)");
+            textBox1.Text = textBox1.Text.Replace("p", "(15)");
+            textBox1.Text = textBox1.Text.Replace("Q", "(16)");
+            textBox1.Text = textBox1.Text.Replace("q", "(16)");
+            textBox1.Text = textBox1.Text.Replace("R", "(17)");
+            textBox1.Text = textBox1.Text.Replace("r", "(17)");
+            textBox1.Text = textBox1.Text.Replace("S", "(18)");
+            textBox1.Text = textBox1.Text.Replace("s", "(18)");
+            textBox1.Text = textBox1.Text.Replace("T", "(19)");
+            textBox1.Text = textBox1.Text.Replace("t", "(19)");
+            textBox1.Text = textBox1.Text.Replace("U", "(20)");
+            textBox1.Text = textBox1.Text.Replace("u", "(20)");
+            textBox1.Text = textBox1.Text.Replace("V", "(21)");
+            textBox1.Text = textBox1.Text.Replace("v", "(21)");
+            textBox1.Text = textBox1.Text.Replace("W", "(22)");
+            textBox1.Text = textBox1.Text.Replace("w", "(22)");
+            textBox1.Text = textBox1.Text.Replace("X", "(23)");
+            textBox1.Text = textBox1.Text.Replace("x", "(23)");
+            textBox1.Text = textBox1.Text.Replace("Y", "(24)");
+            textBox1.Text = textBox1.Text.Replace("y", "(24)");
+            textBox1.Text = textBox1.Text.Replace("Z", "(25)");
+            textBox1.Text = textBox1.Text.Replace("z", "(25)");
+        }
+
+        private void translateText_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("[0]", "A");
+            textBox1.Text = textBox1.Text.Replace("[1]", "B");
+            textBox1.Text = textBox1.Text.Replace("[2]", "C");
+            textBox1.Text = textBox1.Text.Replace("[3]", "D");
+            textBox1.Text = textBox1.Text.Replace("[4]", "E");
+            textBox1.Text = textBox1.Text.Replace("[6]", "G");
+            textBox1.Text = textBox1.Text.Replace("[7]", "H");
+            textBox1.Text = textBox1.Text.Replace("[8]", "I");
+            textBox1.Text = textBox1.Text.Replace("[9]", "J");
+            textBox1.Text = textBox1.Text.Replace("(10)", "K");
+            textBox1.Text = textBox1.Text.Replace("(11)", "L");
+            textBox1.Text = textBox1.Text.Replace("(12)", "M");
+            textBox1.Text = textBox1.Text.Replace("(13)", "N");
+            textBox1.Text = textBox1.Text.Replace("(14)", "O");
+            textBox1.Text = textBox1.Text.Replace("(15)", "P");
+            textBox1.Text = textBox1.Text.Replace("(16)", "Q");
+            textBox1.Text = textBox1.Text.Replace("(17)", "R");
+            textBox1.Text = textBox1.Text.Replace("(18)", "S");
+            textBox1.Text = textBox1.Text.Replace("(19)", "T");
+            textBox1.Text = textBox1.Text.Replace("(20)", "U");
+            textBox1.Text = textBox1.Text.Replace("(21)", "V");
+            textBox1.Text = textBox1.Text.Replace("(22)", "W");
+            textBox1.Text = textBox1.Text.Replace("(23)", "X");
+            textBox1.Text = textBox1.Text.Replace("(24)", "Y");
+            textBox1.Text = textBox1.Text.Replace("(25)", "Z");
+        }
+    }
+}
